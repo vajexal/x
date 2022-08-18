da00ccdaf16a2890bc32dfbb52cd519a514da441
vajexal <vajexal@gmail.com>
2022-08-19 00:14:12 +0300

    abort

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -10,9 +10,13 @@ namespace X {
     void Runtime::addDeclarations(llvm::LLVMContext &context, llvm::Module &module) {
         auto printFnType = llvm::FunctionType::get(llvm::Type::getVoidTy(context), {llvm::Type::getInt64Ty(context)}, false);
         functions["print"] = llvm::dyn_cast<llvm::Function>(module.getOrInsertFunction("print", printFnType).getCallee());
+
+        auto abortFnType = llvm::FunctionType::get(llvm::Type::getVoidTy(context), {}, false);
+        functions["abort"] = llvm::dyn_cast<llvm::Function>(module.getOrInsertFunction("abort", abortFnType).getCallee());
     }
 
     void Runtime::addGlobalMappings(llvm::ExecutionEngine &engine) {
         engine.addGlobalMapping(functions["print"], reinterpret_cast<void *>(_runtime_print));
+        engine.addGlobalMapping(functions["abort"], reinterpret_cast<void *>(std::abort));
     }
 }
